5b8df5c0b461266ec1671dbf81b59cd3e5f2b017
caraclarke <cara.clarke8@gmail.com>
2016-06-05 16:09:57 -0400

    connected to database

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 var express = require('express');
 var morgan = require('morgan');
+var mongoose = require('mongoose');
 
 var app = express();
 var port = 3000;
+
+// connect mongoose to DB
+mongoose.connect('mongodb://root:a@ds023613.mlab.com:23613/ecomm', function(err) {
+  if (err){
+    console.log(err);
+  } else {
+    console.log('connected to DB');
+  }
+})
 
 // invoking morgan object
 // middleware
 app.use(morgan('dev'));
 
+app.get('/', function(req, res) {
+  res.json('face face');
+});
+
 app.listen(port, function(err) {
   if (err) throw err;
   console.log('Server is running on port', port);
 });
-
-// creating request logger
