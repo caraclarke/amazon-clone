45781e9d975e79ae78295dd310a2b6f95a9a94aa
caraclarke <cara.clarke8@gmail.com>
2016-06-05 15:31:12 -0400

    Added morgan middleware
    Added morgan as middleware to log sever requests and status

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var morgan = require('morgan');
 
 var app = express();
 var port = 3000;
+
+// invoking morgan object
+// middleware
+app.use(morgan('dev'));
 
 app.listen(port, function(err) {
   if (err) throw err;
   console.log('Server is running on port', port);
 });
+
+// creating request logger
