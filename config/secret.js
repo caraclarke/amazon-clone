3bbd223dfba2bd4f319aae0cffb798cfe0702c95
caraclarke <cara.clarke8@gmail.com>
2016-06-07 15:00:27 -0400

    added dotenv

--- config/secret.js
+++ config/secret.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 module.exports = {
   database: 'mongodb://root:a@ds023613.mlab.com:23613/ecomm',
   port: 5000,
-  secretKey: 'cara2@3cA'
+  secretKey: process.env.secretKey
 }
