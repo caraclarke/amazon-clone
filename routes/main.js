8183666c7299359e7d505939f25ca51930f0ffe9
caraclarke <cara.clarke8@gmail.com>
2016-06-07 10:21:28 -0400

    Single product page view working

--- routes/main.js
+++ routes/main.js
@@ -11,6 +11,7 @@ router.get('/about', function(req, res) {
   res.render('main/about');
 });
 
+// view all products
 router.get('/products/:id', function(req, res, next) {
   Product
     .find({ category: req.params.id })
@@ -20,7 +21,18 @@ router.get('/products/:id', function(req, res, next) {
 
       res.render('main/category', { products: products });
     })
-})
+});
+
+// view specific product
+router.get('/product/:id', function(req, res, next) {
+  Product.findById({ _id: req.params.id }, function(err, product) {
+    if (err) return next(err);
+
+    res.render('main/product', {
+      product: product
+    });
+  });
+});
 
 
 module.exports = router;
