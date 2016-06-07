1bcc1040398701d8cfc573c7c961ca7d2b07d623
caraclarke <cara.clarke8@gmail.com>
2016-06-07 10:02:59 -0400

    Create route to serve category page

--- routes/main.js
+++ routes/main.js
@@ -10,5 +10,16 @@ router.get('/about', function(req, res) {
   res.render('main/about');
 });
 
+router.get('/products/:id', function(req, res, next) {
+  Product
+    .find({ category: req.params.id })
+    .populate('category')
+    .exec(function(err, products) {
+      if (err) return next(err);
+
+      res.render('main/category', { products: products });
+    })
+})
+
 
 module.exports = router;
