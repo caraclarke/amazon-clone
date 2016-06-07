8d29d4b0bc42ee629bc0c7476b4aa100d38783ab
caraclarke <cara.clarke8@gmail.com>
2016-06-07 14:11:08 -0400

    Added get cart route

--- routes/main.js
+++ routes/main.js
@@ -58,6 +58,19 @@ stream.on('error', function(err) {
 });
 
 // cart routes
+router.get('/cart', function(req, res, next) {
+  Cart
+    .findOne({ owner: req.user._id })
+    .populate('items.item')
+    .exec(function(err, foundCart) {
+      if (err) return next(err);
+
+      res.render('main/cart', {
+        cart: foundCart
+      });
+    });
+});
+
 router.post('/product/:product_id', function(req, res, next) {
   Cart.findOne({ owner: req.user._id }, function(err, cart) {
     cart.items.push({
