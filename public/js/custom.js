96872b961435d922fd748f85d31de5e777321b0e
caraclarke <cara.clarke8@gmail.com>
2016-06-07 14:04:28 -0400

    Can update quantity on product page, price listed in form reflects changes
    Added form to product page, when add or subtract number of items the price reflects that change

--- public/js/custom.js
+++ public/js/custom.js
@@ -37,4 +37,37 @@ $(function() {
     });
   });
 
+  $(document).on('click', '#plus', function(e) {
+    e.preventDefault();
+    var priceValue = parseFloat($('#priceValue').val());
+    var quantity = parseInt($('#quantity').val());
+
+    priceValue += parseFloat($('#priceHidden').val());
+    quantity += 1;
+
+    $('#quantity').val(quantity);
+    $('#priceValue').val(priceValue.toFixed(2));
+    $('#total').html(quantity);
+  });
+
+
+  $(document).on('click', '#minus', function(e) {
+    e.preventDefault();
+    var priceValue = parseFloat($('#priceValue').val());
+    var quantity = parseInt($('#quantity').val());
+
+
+    if (quantity == 1) {
+      priceValue = $('#priceHidden').val();
+      quantity = 1;
+    } else {
+      priceValue -= parseFloat($('#priceHidden').val());
+      quantity -= 1;
+    }
+
+    $('#quantity').val(quantity);
+    $('#priceValue').val(priceValue.toFixed(2));
+    $('#total').html(quantity);
+  });
+
 });
