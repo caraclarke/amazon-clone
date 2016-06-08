aaefd7c887b726fb3788d56044c8e878ac0be49a
caraclarke <cara.clarke8@gmail.com>
2016-06-08 12:41:49 -0400

    Fixed elasticsearch typo in custom js
    Fixed typo for homepage search in custom js that was making the link to inidividual product page incorrect

--- public/js/custom.js
+++ public/js/custom.js
@@ -48,12 +48,11 @@ $(function() {
         for (var i = 0; i < data.length; i++) {
           var html = "";
           html += '<div class="col-md-4">';
-          html += '<a href="/product/' + data[i]._source._id + '">';
+          html += '<a href="/product/' + data[i]._id + '">';
           html += '<div class="thumbnail">';
           html += '<img src="' +  data[i]._source.image + '">';
           html += '<div class="caption">';
           html += '<h3>' + data[i]._source.name  + '</h3>';
-          html += '<p>' +  data[i]._source.category.name  + '</p>'
           html += '<p>$' +  data[i]._source.price  + '</p>';
           html += '</div></div></a></div>';
 
