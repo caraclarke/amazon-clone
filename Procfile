93845ea82ae472be9166a567a92690adf4f773fa
caraclarke <cara.clarke8@gmail.com>
2016-06-08 15:00:08 -0400

    bonsai elasticsearch added

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: nodemon server.js
-worker: elasticsearch
--- server.js
+++ server.js
@@ -10,6 +10,26 @@ var cookieParser = require('cookie-parser');
 var flash = require('express-flash');
 var MongoStore = require('connect-mongo/es5')(session);
 var passport = require('passport');
+var bonsai_url = process.env.BONSAI_URL;
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+  host: bonsai_url,
+  log: 'trace'
+});
+
+// Test the connection...
+client.ping({
+  requestTimeout: 30000,
+  hello: "elasticsearch"
+},
+function (error) {
+  if (error) {
+    console.error('elasticsearch cluster is down!');
+  } else {
+    console.log('All is well');
+  }
+}
+);
 
 require('dotenv').config();
 var secret = require('./config/secret');
