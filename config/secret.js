ce9553057781a807fa2fb8eebede4721a20e7075
caraclarke <cara.clarke8@gmail.com>
2016-06-08 08:46:19 -0400

    Added facebook settings to config file

--- config/secret.js
+++ config/secret.js
@@ -1,6 +1,14 @@
 require('dotenv').config();
+
 module.exports = {
-  database: 'mongodb://root:a@ds023613.mlab.com:23613/ecomm',
+  database: process.env.database,
   port: 5000,
-  secretKey: process.env.secretKey
+  secretKey: process.env.secretKey,
+
+  facebook: {
+    clientId: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    profieField: ['emails', 'displayName'],
+    callbackURL: 'http://localhost:5000/auth/facebook/callback'
+  }
 }
